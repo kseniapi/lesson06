131d116a0f08965970245fd1e38c8983e49a5da0
Kseniia Pikhotnik <pichotnik@gmail.com>
2020-10-16 21:27:42 +0300

    fix: new func

--- script/script.js
+++ script/script.js
@@ -1,38 +1,41 @@
 'use strict';
 const minRange = 1;
 const maxRange = 100;
-const secNum = 54;
 
 const isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+function getRandomArbitrary(min, max) {
+  return parseInt(Math.random() * (max - min) + min);
+}
 
 const gameBot = function () {
-  let secretNumber = secNum;
-
+  const secretNumber = getRandomArbitrary(minRange,maxRange);
+	console.log(secretNumber);
+	
   return function gameBotClosure() {
   	let chosenNumber = prompt(`Угадай число от ${minRange} до ${maxRange}`);
   	if (chosenNumber === null){
   		alert("Игра окончена");
   		return;
   	}
-  	else if(!isNumber(chosenNumber)){
+  	else if (!isNumber(chosenNumber)){
   		alert("Введите число!");
   		gameBotClosure();
   		return;
   	}
-  	else if(chosenNumber < minRange || chosenNumber > maxRange){
+  	else if (chosenNumber < minRange || chosenNumber > maxRange){
   		alert(`Число не в диапазоне от ${minRange} до ${maxRange}`);
   		gameBotClosure();
   		return;
   	}
-  	else if(chosenNumber > secretNumber){
+  	else if (chosenNumber > secretNumber){
   		alert("Загаданное число меньше");
   		gameBotClosure();
   		return;
   	}
-  	else if(chosenNumber < secretNumber){
+  	else if (chosenNumber < secretNumber){
   		alert("Загаданное число больше");
   		gameBotClosure();
   		return;
